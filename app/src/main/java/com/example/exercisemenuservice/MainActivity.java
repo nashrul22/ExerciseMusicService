7685cc55f416b8cd9c40308909c9d02254e0b738
Nashrul Saefullah <nashrul.saefullah.ft18@mail.umy.ac.id>
2020-05-10 15:01:59 +0700

    mengkonfigurasi pemutar musik

--- app/src/main/java/com/example/exercisemenuservice/MainActivity.java
+++ app/src/main/java/com/example/exercisemenuservice/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.exercisemenuservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.exercisemenuservice.MusicService.MusicBinder;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.widget.ListView;
+import android.content.Context;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Music> songList;
     private ListView songView;
+
+
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent==null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicBinder binder = (MusicBinder)service;
+
+            musicSrv = binder.getService();
+
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+musicBound = false;
+        }
+    };
 
 
     public void getMusicList(){
--- app/src/main/java/com/example/exercisemenuservice/MusicService.java
+++ app/src/main/java/com/example/exercisemenuservice/MusicService.java
@@ -2,8 +2,11 @@ package com.example.exercisemenuservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,28 @@ public class MusicService extends Service implements
     private ArrayList<Music> songs;
     private int songPosn;
 
+
+    public void initMusicPlayer(){
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setList(ArrayList<Music> theSongs){
+        songs = theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService(){
+            return MusicService.this;
+        }
+    }
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent arg0) {
@@ -28,7 +53,11 @@ public class MusicService extends Service implements
         super.onCreate();
         songPosn=0;
         player = new MediaPlayer();
+
+        initMusicPlayer();
     }
+
+
 
     @Override
     public void onCompletion(MediaPlayer mp) {
