fa8f4a560f45276749a138984d656cdde3180d09
Nashrul Saefullah <nashrul.saefullah.ft18@mail.umy.ac.id>
2020-05-10 09:09:05 +0700

    membuat class musik untuk mencari lagu pada device

---
+++ app/src/main/java/com/example/exercisemenuservice/Music.java
@@ -0,0 +1,21 @@
+package com.example.exercisemenuservice;
+
+
+
+public class Music {
+    private long id;
+    private String title;
+    private String artis;
+
+    public Music(long songID, String songTitle, String songArtis){
+        id = songID;
+        title = songTitle;
+        artis = songArtis;
+
+
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtis(){return artis;}
+}
