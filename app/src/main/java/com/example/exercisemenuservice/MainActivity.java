5ad7ea558e75b9911d6b28324afec3713a4d6bed
Nashrul Saefullah <nashrul.saefullah.ft18@mail.umy.ac.id>
2020-05-10 09:25:24 +0700

    mencari lagu pada perangkat

--- app/src/main/java/com/example/exercisemenuservice/MainActivity.java
+++ app/src/main/java/com/example/exercisemenuservice/MainActivity.java
@@ -2,21 +2,53 @@ package com.example.exercisemenuservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ArrayList<Music> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        songView = (ListView) findViewById(R.id.listSong);
+        songList = new ArrayList<Music>();
 
+    }
 
+    public void getMusicList(){
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if (musicCursor != null && musicCursor.moveToFirst()){
+            int titleColumn = musicCursor.getColumnIndex(
+                    MediaStore.Audio.Media.TITLE
+            );
+            int idColumn = musicCursor.getColumnIndex(
+                    MediaStore.Audio.Media._ID
+            );
+            int artisColumn = musicCursor.getColumnIndex(
+                    MediaStore.Audio.Media.ARTIST
+            );
+
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtis = musicCursor.getString(artisColumn);
+                songList.add(new Music(thisId, thisTitle, thisArtis));
+            }
+            while (musicCursor.moveToNext());
+        }
     }
 }
